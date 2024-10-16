c7d9ec3acccb53bc918fd76899decfea0eefe7c9
Eduardo Matuella <eduardomatuella@gmail.com>
2024-10-16 16:35:24 -0300

    altering project name and remove os and sys requirements

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="recipe_manager",
+    name="recipe_manager_montwh1te",
     version="0.0.1",
     author="montwh1te",
     author_email="eduardomatuella@gmail.com",
